a722dd9cc21944f2cc41ed27faaceda3129541fd
Nikitos787 <nsalogub96@gmail.com>
2026-03-31 23:46:16 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
-import { HomePage } from './components/HomePage';
+import { Routes, Route, Navigate, NavLink } from 'react-router-dom';
+import classNames from 'classnames';
 import { TabsPage } from './components/TabsPage';
-
-const tabs = [
-  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-];
 
 export const App = () => (
   <>
-    {/* Also requires <html class="has-navbar-fixed-top"> */}
     <nav
       className="navbar is-light is-fixed-top is-mobile has-shadow"
       data-cy="Nav"
@@ -22,18 +15,16 @@ export const App = () => (
         <div className="navbar-brand">
           <NavLink
             to="/"
-            end
             className={({ isActive }) =>
-              'navbar-item' + (isActive ? ' is-active' : '')
+              classNames('navbar-item', { 'is-active': isActive })
             }
           >
             Home
           </NavLink>
-
           <NavLink
             to="/tabs"
             className={({ isActive }) =>
-              'navbar-item' + (isActive ? ' is-active' : '')
+              classNames('navbar-item', { 'is-active': isActive })
             }
           >
             Tabs
@@ -42,23 +33,18 @@ export const App = () => (
       </div>
     </nav>
 
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/home" element={<Navigate to="/" />} />
-      <Route path="/tabs">
-        <Route index element={<TabsPage tabs={tabs} />} />
-        <Route path=":tabId" element={<TabsPage tabs={tabs} />} />
-      </Route>
-      <Route
-        path="*"
-        element={
-          <div className="section">
-            <div className="container">
-              <h1 className="title">Page not found</h1>
-            </div>
-          </div>
-        }
-      />
-    </Routes>
+    <div className="section">
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<h1 className="title">Home page</h1>} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+          <Route path="tabs">
+            <Route index element={<TabsPage />} />
+            <Route path=":tabId" element={<TabsPage />} />
+          </Route>
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+        </Routes>
+      </div>
+    </div>
   </>
 );
--- src/components/TabsPage.tsx
+++ src/components/TabsPage.tsx
@@ -1,36 +1,15 @@
-import { Link, useParams } from 'react-router-dom';
-import { Tab } from '../types/Tab';
+import { Tabs } from '../components/Tabs';
 
-type Props = {
-  tabs: Tab[];
-};
+const tabs = [
+  { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
+  { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
+  { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
+];
 
-export const TabsPage = ({ tabs }: Props) => {
-  const { tabId } = useParams();
-  const currentTab = tabs.find(tab => tab.id === tabId);
+export const TabsPage = () => (
+  <>
+    <h1 className="title">Tabs page</h1>
 
-  return (
-    <div className="section">
-      <div className="container">
-        <h1 className="title">Tabs page</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            {tabs.map(tab => (
-              <li
-                data-cy="Tab"
-                key={tab.id}
-                className={tab.id === tabId ? 'is-active' : ''}
-              >
-                <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="block" data-cy="TabContent">
-          {!tabId || !currentTab ? 'Please select a tab' : currentTab.content}
-        </div>
-      </div>
-    </div>
-  );
-};
+    <Tabs tabs={tabs} />
+  </>
+);
